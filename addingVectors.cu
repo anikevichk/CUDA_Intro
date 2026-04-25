3f3d22596f57093254d1240af0c5518cfff21ace
Katsyaryna Anikevich <anick6916@gmail.com>
2026-04-25 13:39:48 +0200

    - Reduced block size to improve theoretical occupancy from 66.7% to 100% - Added CUDA and host allocation error checks - Added device properties and SM limits utilities

---
+++ addingVectors.cu
@@ -0,0 +1,109 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE (1024LL * 1024 * 1024)
+#define CHUNK_SIZE (1024 * 1024 * 128)
+#define BLOCK_SIZE 256
+
+#define CUDA_CHECK(call) do {                                      \
+    cudaError_t err = call;                                        \
+    if (err != cudaSuccess) {                                      \
+        fprintf(stderr, "CUDA error at %s:%d: %s\n",               \
+                __FILE__, __LINE__, cudaGetErrorString(err));      \
+        exit(EXIT_FAILURE);                                        \
+    }                                                              \
+} while (0)
+
+#define HOST_CHECK(ptr) do {                                       \
+    if ((ptr) == NULL) {                                           \
+        fprintf(stderr, "Host malloc failed at %s:%d\n",           \
+                __FILE__, __LINE__);                               \
+        exit(EXIT_FAILURE);                                        \
+    }                                                              \
+} while (0)
+
+__global__ void vectorAdd(int* A, int* B, int* C, int n){
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    if (index < n){
+        C[index] = A[index] + B[index];
+    }
+}
+
+void randomNum(int* x, int size){
+    for (int i = 0; i < size; i++){
+        x[i] = rand() % 100;
+    }
+}
+
+int main(){
+    int* chunk_a; int* chunk_b; int* chunk_c;
+    int* d_A; int* d_B; int* d_C;
+
+    size_t maxChunkBytes = CHUNK_SIZE * sizeof(int);
+
+    chunk_a = (int*)malloc(maxChunkBytes);
+    chunk_b = (int*)malloc(maxChunkBytes);
+    chunk_c = (int*)malloc(maxChunkBytes);
+
+    HOST_CHECK(chunk_a);
+    HOST_CHECK(chunk_b);
+    HOST_CHECK(chunk_c);
+
+    CUDA_CHECK(cudaMalloc((void**)&d_A, maxChunkBytes));
+    CUDA_CHECK(cudaMalloc((void**)&d_B, maxChunkBytes));
+    CUDA_CHECK(cudaMalloc((void**)&d_C, maxChunkBytes));
+
+    cudaEvent_t start, stop;
+    CUDA_CHECK(cudaEventCreate(&start));
+    CUDA_CHECK(cudaEventCreate(&stop));
+
+    CUDA_CHECK(cudaEventRecord(start));
+
+    for (long long offset = 0; offset < SIZE; offset += CHUNK_SIZE){
+        int currentChunkSize = (int)((SIZE - offset < CHUNK_SIZE)
+            ? (SIZE - offset)
+            : CHUNK_SIZE);
+
+        size_t currentBytes = currentChunkSize * sizeof(int);
+
+        randomNum(chunk_a, currentChunkSize);
+        randomNum(chunk_b, currentChunkSize);
+
+        CUDA_CHECK(cudaMemcpy(d_A, chunk_a, currentBytes, cudaMemcpyHostToDevice));
+        CUDA_CHECK(cudaMemcpy(d_B, chunk_b, currentBytes, cudaMemcpyHostToDevice));
+
+        int numBlocks = (currentChunkSize + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+        vectorAdd<<<numBlocks, BLOCK_SIZE>>>(d_A, d_B, d_C, currentChunkSize);
+
+        CUDA_CHECK(cudaGetLastError());
+        CUDA_CHECK(cudaDeviceSynchronize());
+
+        CUDA_CHECK(cudaMemcpy(chunk_c, d_C, currentBytes, cudaMemcpyDeviceToHost));
+
+        printf("Processed chunk offset: %lld / %lld\n", offset, SIZE);
+    }
+
+    CUDA_CHECK(cudaEventRecord(stop));
+    CUDA_CHECK(cudaEventSynchronize(stop));
+
+    float milliseconds = 0;
+    CUDA_CHECK(cudaEventElapsedTime(&milliseconds, start, stop));
+
+    printf("Total execution time: %f ms\n", milliseconds);
+
+    CUDA_CHECK(cudaFree(d_A));
+    CUDA_CHECK(cudaFree(d_B));
+    CUDA_CHECK(cudaFree(d_C));
+
+    free(chunk_a);
+    free(chunk_b);
+    free(chunk_c);
+
+    CUDA_CHECK(cudaEventDestroy(start));
+    CUDA_CHECK(cudaEventDestroy(stop));
+
+    return 0;
+}
---
+++ deviceInfo/GPUInfo.cu
@@ -0,0 +1,54 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+
+int main()
+{
+    int nDevices;
+    cudaGetDeviceCount(&nDevices);
+
+    for (int i = 0; i < nDevices; i++)
+    {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+
+        int memoryClockRate;
+        cudaDeviceGetAttribute(&memoryClockRate, cudaDevAttrMemoryClockRate, i);
+
+        printf("Device Number: %d\n", i);
+        printf("  Device name: %s\n", prop.name);
+
+        printf("  Memory Clock Rate (KHz): %d\n",
+            memoryClockRate);
+
+        printf("  Memory Bus Width (bits): %d\n",
+            prop.memoryBusWidth);
+
+        printf("  Peak Memory Bandwidth (GB/s): %f\n",
+            2.0 * memoryClockRate * (prop.memoryBusWidth / 8) / 1.0e6);
+
+        printf("  Total global memory: %llu\n",
+            (unsigned long long)prop.totalGlobalMem);
+
+        printf("  Compute capability: %d.%d\n",
+            prop.major, prop.minor);
+
+        printf("  Number of SMs: %d\n",
+            prop.multiProcessorCount);
+
+        printf("  Max threads per block: %d\n",
+            prop.maxThreadsPerBlock);
+
+        printf("  Max threads dimensions: x = %d, y = %d, z = %d\n",
+            prop.maxThreadsDim[0],
+            prop.maxThreadsDim[1],
+            prop.maxThreadsDim[2]);
+
+        printf("  Max grid dimensions: x = %d, y = %d, z = %d\n",
+            prop.maxGridSize[0],
+            prop.maxGridSize[1],
+            prop.maxGridSize[2]);
+    }
+
+    return 0;
+}
